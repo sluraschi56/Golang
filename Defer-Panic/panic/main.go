82bb0a945362fc714d8ae317c99b6cae93571a89
sluraschi56 <sluraschi56@hotmail.com>
2021-01-26 16:46:31 -0300

    Panic

--- Defer-Panic/panic/main.go
+++ Defer-Panic/panic/main.go
@@ -1,15 +1,15 @@
 package main
 
-import (
-	"net/http"
-)
+import "fmt"
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hola Mundo!"))
-	})
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		panic(err.Error())
-	}
+	//	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { w.Write([]byte("Hola Mundo!")) })
+	//	err := http.ListenAndServe(":8080", nil)
+	//	if err != nil {
+	//		panic(err.Error())
+	//	}
+
+	fmt.Println("start")
+	panic("something bad happened") //fmt.Println("middle")
+	fmt.Println("end")
 }
